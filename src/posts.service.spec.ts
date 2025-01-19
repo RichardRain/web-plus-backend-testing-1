a154e3bee9df1ae042ec85c9dc76c49eef159cad
RichardRain <alexander.hell.scorpion@gmail.com>
2025-01-19 17:43:00 +0300

    feat: Реализованы тесты для проверки работы PostService

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,34 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
-  let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
-  };
+describe("PostsService", () => {
+	let postsService: PostsService;
+	const post: Omit<Post, "id" | "date"> = {
+		text: "Mocked post",
+	};
 
-  beforeEach(async () => {
-    postsService = new PostsService();
+	beforeEach(async () => {
+		postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
-  });
+		postsService.create({ text: "Some pre-existing post" });
+	});
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
-  });
+	it("should add a new post", () => {
+		const newPost = postsService.create(post);
+    const newPostDate = new Date(newPost.date);
+		expect(newPost.id).toEqual("2");
+		expect(newPost.text).toEqual(post.text);
+		expect(newPostDate).toBeInstanceOf(Date);
+	});
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
-  });
+	it("should find a post", () => {
+		const foundPost = postsService.find("1");
+		if (foundPost) {
+      const foundPostDate = new Date(foundPost.date);
+			expect(foundPost.id).toEqual("1");
+			expect(foundPost.text).toEqual("Some pre-existing post");
+			expect(foundPostDate).toBeInstanceOf(Date);
+		} else {
+			fail("Post not found");
+		}
+	});
 });
